1b49646faa0ed71af9c696cdb90370193116e8dc
peterumimo <peterumimo2@gmail.com>
2022-11-02 10:57:17 +0100

    Customer and Vendor Registration

--- account/models.py
+++ account/models.py
@@ -81,6 +81,13 @@ class User(AbstractBaseUser):
     def has_module_perms(self, app_label):
         return True
 
+    def get_role(self):
+        if self.role == 1:
+            user_role = 'Vendor'
+        elif self.role == 2:
+            user_role = 'Customer'
+        return user_role
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE, null=True, blank=True )
     profile_picture = models.ImageField(upload_to='users/profile_picture', blank=True, null=True)
